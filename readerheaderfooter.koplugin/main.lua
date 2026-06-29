0701f0e0c6d6ca018c2de7c49083bbfec306e479
Djeymisson Moraes <djeymisson@gmail.com>
2026-06-29 17:25:10 -0300

    change button text

--- readerheaderfooter.koplugin/main.lua
+++ readerheaderfooter.koplugin/main.lua
@@ -378,7 +378,7 @@ function ReaderHeaderFooter:addToMainMenu(menu_items)
 		sub_item_table = {
 			{
 				text_func = function()
-					return self:isEnabled() and _("Disable plugin") or _("Enable plugin")
+					return self:isEnabled() and _("Show header/footer") or _("Hide header/footer")
 				end,
 
 				checked_func = function()
